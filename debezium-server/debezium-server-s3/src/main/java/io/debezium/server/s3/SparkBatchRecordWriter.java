869631abb5b8eebd51ea472d081145ca6e527ae6
Ismail Simsek <ismail.simsek@ppro.com>
2020-08-03 19:53:17 +0200

    WIP use abstract class

--- debezium-server/debezium-server-s3/src/main/java/io/debezium/server/s3/SparkBatchRecordWriter.java
+++ debezium-server/debezium-server-s3/src/main/java/io/debezium/server/s3/SparkBatchRecordWriter.java
@@ -12,7 +12,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Iterables;
 import org.apache.commons.io.IOUtils;
 import org.apache.spark.SparkConf;
-import org.apache.spark.sql.*;
+import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.StructType;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 
