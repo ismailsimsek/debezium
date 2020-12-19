64af575ca4f6bf246df10df0658829001f0758c4
Ismail Simsek <ismail.simsek@ppro.com>
2020-12-19 17:56:51 +0100

    restructure

---
+++ debezium-server/debezium-server-batch/src/main/java/io/debezium/server/iceberg/IcebergEventsChangeConsumer.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.server.iceberg;
+
+import io.debezium.engine.ChangeEvent;
+import io.debezium.engine.DebeziumEngine;
+import io.debezium.server.BaseChangeConsumer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.DataFile;
+import org.apache.iceberg.DataFiles;
+import org.apache.iceberg.FileFormat;
+import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.catalog.Catalog;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.data.GenericRecord;
+import org.apache.iceberg.data.Record;
+import org.apache.iceberg.data.parquet.GenericParquetWriter;
+import org.apache.iceberg.hadoop.HadoopCatalog;
+import org.apache.iceberg.io.FileAppender;
+import org.apache.iceberg.io.OutputFile;
+import org.apache.iceberg.parquet.Parquet;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.types.Types;
+import static org.apache.iceberg.types.Types.NestedField.optional;
+import static org.apache.iceberg.types.Types.NestedField.required;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Implementation of the consumer that delivers the messages into Amazon S3 destination.
+ *
+ * @author Ismail Simsek
+ */
+@Named("icebergevents")
+@Dependent
+public class IcebergEventsChangeConsumer extends BaseChangeConsumer implements DebeziumEngine.ChangeConsumer<ChangeEvent<Object, Object>> {
+
+    static final String TABLE_NAME = "debezium_events";
+    static final Schema TABLE_SCHEMA = new Schema(
+            required(1, "event_destination", Types.StringType.get(), "event destination"),
+            optional(2, "event_key", Types.StringType.get()),
+            optional(3, "event_key_value", Types.StringType.get()),
+            optional(4, "event_value", Types.StringType.get()),
+            optional(5, "event_value_format", Types.StringType.get()),
+            optional(6, "event_key_format", Types.StringType.get()),
+            optional(7, "event_sink_timestamp", Types.TimestampType.withZone()));
+    // debezium.format.value.schemas.enable
+    // debezium.format.key.schemas.enable
+    // debezium.source.database.dbname - database
+    // partition by event_destination,event_sink_timestamp
+    // extract key value from json!
+    static final PartitionSpec TABLE_PARTITION = PartitionSpec.builderFor(TABLE_SCHEMA).identity("event_destination").day("event_sink_timestamp").build();
+    private static final Logger LOGGER = LoggerFactory.getLogger(IcebergEventsChangeConsumer.class);
+    private static final String PROP_PREFIX = "debezium.sink.iceberg.";
+
+    // @ConfigProperty(name = "debezium.sink.iceberg.bucket.name", defaultValue = "My-S3-Bucket")
+    // String bucket;
+    @ConfigProperty(name = "debezium.format.value")
+    String valueFormat;
+    @ConfigProperty(name = "debezium.format.key")
+    String keyFormat;
+    Configuration hadoopConf = new Configuration();
+    @ConfigProperty(name = "xxxxx??xxxx", defaultValue = "My-DB")
+    String database;
+    //@ConfigProperty(name = PROP_PREFIX + CatalogProperties.CATALOG_IMPL, defaultValue = "set to haddop")
+    //String catalogImpl;
+    @ConfigProperty(name = PROP_PREFIX + "warehouse" /*CatalogProperties.WAREHOUSE_LOCATION*/)
+    String warehouseLocation;
+    @ConfigProperty(name = PROP_PREFIX + "fs.defaultFS")
+    String defaultFs;
+    final Integer batchLimit = ConfigProvider.getConfig().getOptionalValue("debezium.sink.batch.row.limit", Integer.class).orElse(500);
+
+    // HadoopTables hadoopTables;
+    Catalog icebergCatalog;
+    Table eventTable;
+
+    @PostConstruct
+    void connect() throws URISyntaxException {
+        // loop and set hadoopConf
+        for (String name : ConfigProvider.getConfig().getPropertyNames()) {
+            if (name.startsWith(PROP_PREFIX)) {
+                this.hadoopConf.set(name.substring(PROP_PREFIX.length()), ConfigProvider.getConfig().getValue(name, String.class));
+                LOGGER.debug("Setting Hadoop Conf '{}'='{}'", name.substring(PROP_PREFIX.length()), ConfigProvider.getConfig().getValue(name, String.class));
+            }
+        }
+
+        if (warehouseLocation == null || warehouseLocation.trim().isEmpty()) {
+            warehouseLocation = defaultFs + "/iceberg/warehouse";
+        }
+        icebergCatalog = new HadoopCatalog("iceberg", hadoopConf, warehouseLocation);
+
+        if (!icebergCatalog.tableExists(TableIdentifier.of(TABLE_NAME))) {
+            icebergCatalog.createTable(TableIdentifier.of(TABLE_NAME), TABLE_SCHEMA, TABLE_PARTITION);
+        }
+        eventTable = icebergCatalog.loadTable(TableIdentifier.of(TABLE_NAME));
+        // hadoopTables = new HadoopTables(hadoopConf);// do we need this ??
+        // @TODO iceberg 11
+        //if (catalogImpl != null) {
+        //    icebergCatalog = CatalogUtil.loadCatalog(catalogImpl, name, options, hadoopConf);
+        //}
+    }
+
+    @PreDestroy
+    void close() {
+        // try {
+        // s3client.close();
+        // }
+        // catch (Exception e) {
+        // LOGGER.error("Exception while closing S3 client: ", e);
+        // }
+    }
+
+    @Override
+    public void handleBatch(List<ChangeEvent<Object, Object>> records, DebeziumEngine.RecordCommitter<ChangeEvent<Object, Object>> committer)
+            throws InterruptedException {
+        LocalDateTime batchTime = LocalDateTime.now();
+        ArrayList<Record> icebergRecords = Lists.newArrayList();
+        GenericRecord icebergRecord = GenericRecord.create(TABLE_SCHEMA);
+        int batchId = 0;
+        int cntNumRows = 0;
+        for (ChangeEvent<Object, Object> record : records) {
+            LOGGER.debug("key:{}", record.key());
+            LOGGER.debug("value:{}", record.value());
+            LOGGER.debug("dest:{}", record.destination());
+            // @TODO
+            icebergRecords.add(icebergRecord.copy("age", 29L, "name", "GenericRecord-a"));
+//            required(1, "event_destination", Types.StringType.get(), "event destination"),
+//            optional(2, "event_key", Types.StringType.get()),
+//            optional(3, "event_key_value", Types.StringType.get()),
+//            optional(4, "event_value", Types.StringType.get()),
+//            optional(5, "event_value_format", Types.StringType.get()),
+//            optional(6, "event_key_format", Types.StringType.get()),
+//            optional(7, "event_sink_timestamp", Types.TimestampType.withZone()));
+
+            cntNumRows++;
+            if (cntNumRows > batchLimit) {
+                commitBatch(icebergRecords, batchTime, batchId);
+                cntNumRows = 0;
+                batchId++;
+                icebergRecords.clear();
+            }
+            // committer.markProcessed(record);
+        }
+        commitBatch(icebergRecords, batchTime, batchId);
+        icebergRecords.clear();
+        committer.markBatchFinished();
+    }
+
+    private void commitBatch(ArrayList<Record> icebergRecords, LocalDateTime batchTime, int batchId) throws InterruptedException {
+        final String fileName = UUID.randomUUID() + "-" + batchTime.toEpochSecond(ZoneOffset.UTC) + "-" + batchId;
+        OutputFile out = eventTable.io().newOutputFile(eventTable.locationProvider().newDataLocation(fileName));
+
+        FileAppender<Record> writer = null;
+        try {
+            writer = Parquet.write(out)
+                    .createWriterFunc(GenericParquetWriter::buildWriter)
+                    .forTable(eventTable)
+                    .overwrite()
+                    .build();
+
+            try (Closeable toClose = writer) {
+                writer.addAll(icebergRecords);
+            }
+
+        }
+        catch (IOException e) {
+            LOGGER.error("Failed committing events to iceberg table!", e);
+            throw new InterruptedException(e.getMessage());
+        }
+
+        DataFile dataFile = DataFiles.builder(eventTable.spec())
+                .withFormat(FileFormat.PARQUET)
+                .withPath(out.location())
+                .withFileSizeInBytes(writer.length())
+                .withSplitOffsets(writer.splitOffsets())
+                .withMetrics(writer.metrics())
+                .build();
+
+        LOGGER.debug("Appending new file '{}' !", dataFile.path());
+        eventTable.newAppend()
+                .appendFile(dataFile)
+                .commit();
+        LOGGER.info("Committed events to table! {}", eventTable.location());
+    }
+
+}
---
+++ debezium-server/debezium-server-batch/src/test/java/io/debezium/server/iceberg/IcebergEventsIT.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.server.iceberg;
+
+import io.debezium.server.DebeziumServer;
+import io.debezium.server.TestDatabase;
+import io.debezium.server.batch.ConfigSource;
+import io.debezium.server.batch.S3MinioServer;
+import io.debezium.server.events.ConnectorCompletedEvent;
+import io.debezium.server.events.ConnectorStartedEvent;
+import io.debezium.util.Testing;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidResponseException;
+import io.minio.errors.ServerException;
+import io.minio.errors.XmlParserException;
+import io.minio.messages.Item;
+import io.quarkus.test.junit.QuarkusTest;
+import org.awaitility.Awaitility;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.fest.assertions.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
+
+/**
+ * Integration test that verifies basic reading from PostgreSQL database and writing to s3.
+ *
+ * @author Ismail Simsek
+ */
+@QuarkusTest
+public class IcebergEventsIT {
+
+    protected static final S3MinioServer s3server = new S3MinioServer();
+    protected static TestDatabase db;
+
+    static {
+        Testing.Files.delete(ConfigSource.OFFSET_STORE_PATH);
+        Testing.Files.createTestingFile(ConfigSource.OFFSET_STORE_PATH);
+    }
+
+    @Inject
+    DebeziumServer server;
+    @ConfigProperty(name = "debezium.sink.type")
+    String sinkType;
+
+    {
+        // Testing.Debug.enable();
+        Testing.Files.delete(ConfigSource.OFFSET_STORE_PATH);
+        Testing.Files.createTestingFile(ConfigSource.OFFSET_STORE_PATH);
+    }
+
+    @AfterAll
+    static void stop() {
+        if (db != null) {
+            db.stop();
+        }
+        s3server.stop();
+    }
+
+    @BeforeAll
+    static void setUpS3()
+            throws XmlParserException, InsufficientDataException, ServerException,
+            InternalException, InvalidResponseException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException,
+            KeyManagementException {
+        s3server.start();
+    }
+
+    void setupDependencies(@Observes ConnectorStartedEvent event) throws URISyntaxException {
+        if (!sinkType.equals("icebergevents")) {
+            return;
+        }
+        db = new TestDatabase();
+        //db.start();
+    }
+
+    void connectorCompleted(@Observes ConnectorCompletedEvent event) throws Exception {
+        if (!event.isSuccess()) {
+            throw (Exception) event.getError().get();
+        }
+    }
+
+    @Test
+    public void testS3() throws Exception {
+        Testing.Print.enable();
+        Assertions.assertThat(sinkType.equals("icebergevents"));
+
+        Awaitility.await().atMost(Duration.ofSeconds(ConfigSource.waitForSeconds())).until(() -> {
+            //Testing.printError(s3server.getObjectList(ConfigSource.S3_BUCKET));
+            //s3server.listFiles();
+            return s3server.getObjectList(ConfigSource.S3_BUCKET).size() >= 16;
+        });
+        s3server.listFiles();
+    }
+}
